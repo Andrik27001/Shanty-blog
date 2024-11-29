9c42260dac9f3186151646bb295dbd3417831c12
Andrik Solis <andrik.solis@weareinsolite.com>
2024-11-29 19:58:07 +0100

    Created featured recipe section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "sky-blue": "#E7FAFE",
+        "light-gray": "rgb(0,0,0, 0.6)",
+      },
+    },
+  },
+  plugins: [],
+};
