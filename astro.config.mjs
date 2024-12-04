ab7e882cacd5d4ccea92a0067c59f0152e4edb8c
Andrik Solis <andrik.solis@weareinsolite.com>
2024-12-04 11:05:58 +0100

    Fixed markdown conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), keystatic(), react()],
+  integrations: [tailwind(), keystatic(), react(), markdoc()],
 })
